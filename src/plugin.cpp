252215de661dd9fe4d21410b1146a1fd18d8bbdc
gabriel-roth <gabe.roth@gmail.com>
2026-06-11 12:08:44 -0400

    Support building as a MetaModule simulator built-in

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,15 +1,23 @@
 #include "plugin.hpp"
 
 
+#ifdef METAMODULE_BUILTIN
+extern Plugin* pluginInstance;
+#else
 Plugin* pluginInstance;
+#endif
 
 
+#ifdef METAMODULE_BUILTIN
+void init_CuteLab(Plugin* p) {
+#else
 void init(Plugin* p) {
+#endif
 	pluginInstance = p;
 
 	// Add modules here
 	p->addModel(modelMom_jeans);
-	
+
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
